449cb70912b3c3f4b21973f15c7d0fe223fb65e2
qiaozhiyuan <18226851365@163.com>
2021-09-23 15:05:53 +0800

    修改-项目-配置

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,19 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
-import axios from '../utils/axios';
+import { defineComponent } from 'vue'
+import axios from '../utils/axios'
 
 export default defineComponent({
   setup() {
     axios
       .get('/users/XPoet')
       .then((res) => {
-        console.log('res:', res);
+        console.log('res:', res)
       })
       .catch((err) => {
-        console.log('err:', err);
-      });
-  },
-});
+        console.log('err:', err)
+      })
+  }
+})
 </script>
 
 <template>
