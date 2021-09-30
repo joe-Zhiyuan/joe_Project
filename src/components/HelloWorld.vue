b02d3a640fd31b5b2acb5d07e03423e558e330f5
qiaozhiyuan <18226851365@163.com>
2021-09-30 15:17:34 +0800

    feat(styles): 新增element-plus应用

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,7 +17,7 @@ export default defineComponent({
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <!-- <h1>{{ msg }}</h1> -->
 
   <p>
     Recommended IDE setup:
@@ -34,7 +34,7 @@ export default defineComponent({
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
+  <!-- <button type="button" @click="count++">count is: {{ count }}</button> -->
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,14 @@
 import { createApp } from 'vue'
-
+// 挂载Element Plus
 import ElementPlus from 'element-plus'
-
+import 'element-plus/dist/index.css'
+// import ElementPlus from 'element-plus'
 import App from './App.vue'
-
 // 挂载路由配置
-
 import router from './router/index'
-
 // 挂载Vuex配置
-
 import store from './store/index'
 
-// 挂载Element Plus
-
-// import 'element-plus/lib/theme-chalk/index.css'
-
-createApp(App).use(router).use(store).use(ElementPlus).mount('#app')
+createApp(App).use(ElementPlus).use(router).use(store).mount('#app')
 
 // createApp(App).use(store).mount('#app')
