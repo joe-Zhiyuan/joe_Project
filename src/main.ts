fafd8823c7c3a15476ca46c0771934f46068fdf9
qiaozhiyuan <18226851365@163.com>
2021-09-30 11:17:22 +0800

    build(other): 打包问题修改

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { createApp } from 'vue'
+
 import ElementPlus from 'element-plus'
+
 import App from './App.vue'
+
 // 挂载路由配置
+
 import router from './router/index'
+
 // 挂载Vuex配置
+
 import store from './store/index'
+
 // 挂载Element Plus
+
 // import 'element-plus/lib/theme-chalk/index.css'
 
-createApp(App).use(router, store, ElementPlus).mount('#app')
+createApp(App).use(router).use(store).use(ElementPlus).mount('#app')
+
 // createApp(App).use(store).mount('#app')
