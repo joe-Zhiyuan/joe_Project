41bc09fedc4a9a6326ab06f08b533eec0f224db7
joe-Zhiyuan <joe-Zhiyuan@users.noreply.github.com>
2021-10-09 02:46:13 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter 构建 7c50f7c55cb8da59b1462a48d1377e5b527538f1

--- commitlint.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = { extends: ['@commitlint/config-conventional'] }
-
-// 使用 commitlint 时报错：commitlint.config.js:1 SyntaxError: Invalid or unexpected token
-
-// 原来是因为用 echo "module.exports = {extends: ['@commitlint/config-conventional']}" > commitlint.config.js 这条命令
-
-// 生成的 commitlint.config.js 文件不是 utf8 格式的，将文件转成 utf8 格式的就没问题
